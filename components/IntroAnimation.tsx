c84b24e48ec3bf26fb2f14077719c5f0f096bc8d
Faris Nur Adlin <farisadlin@Fariss-MacBook-Pro.local>
2023-05-07 17:43:58 +0700

    feat: added new logo on the front side

--- components/IntroAnimation.tsx
+++ components/IntroAnimation.tsx
@@ -1,8 +1,11 @@
+import Image from 'next/image'
 import React from 'react'
+import Logo from '../styles/logo.svg'
 
 const IntroAnimation = ({animationFinished, handleAnimationEnd}) => {
   return (
     <section id='introAnimation'>
+      <Image className={`cursor-pointer z-20 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-48 ${animationFinished ? 'animate-fade-out' : 'animate-top-to-bottom'}`} src={Logo} width={150} height={150} alt='Logo' />
       <p 
       className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-8xl ${animationFinished ? 'animate-fade-out' : 'animate-left-to-right'}`} 
       onAnimationEnd={handleAnimationEnd}
@@ -12,9 +15,9 @@ const IntroAnimation = ({animationFinished, handleAnimationEnd}) => {
       <p 
       className={`absolute top-1/2 left-1/2 -translate-x-1/2 translate-y-10 text-8xl ${animationFinished ? 'animate-fade-out' : 'animate-right-to-left'}`}
       >
-      <span className='text-secondary animate-change-text-color'>A</span>
+        <span className='text-secondary animate-change-text-color'>A</span>
         DLIN
-        </p>
+      </p>
     </section>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,10 @@ module.exports = {
       animation: {
         'change-text-color': 'change-text-color 2s ease-in-out',
         'fade-out': 'fade-out 1s ease-in-out',
+        'intro-logo-dash': 'intro-logo-dash 2s linear infinite',
         'left-to-right': 'left-to-right 2s ease-in-out',
-        'right-to-left': 'right-to-left 2s ease-in-out'
+        'right-to-left': 'right-to-left 2s ease-in-out',
+        'top-to-bottom': 'top-to-bottom 2s ease-in-out'
       },
       boxShadow: {
         'btn-resume': '3px 3px 0 0 #D926AA',
@@ -21,6 +23,10 @@ module.exports = {
         'fira-code': ['Fira Code', 'monospace']
       },
       keyframes: {
+        'intro-logo-dash': {
+          '0%': { 'stroke-dashoffset': 1 },
+          '100%': { 'stroke-dashoffset': 0 }
+        },
         'fade-out': {
           '0%': { opacity: '1' },
           '100%': { opacity: '0' }
@@ -33,9 +39,19 @@ module.exports = {
           '0%': { left: '125%' },
           '100%': { left: '50%' }
         },
+        'top-to-bottom': {
+          '0%': { top: '0' },
+          '100%': { top: '50%' }
+        },
         'change-text-color': {
           '0%': { color: '#A6ADBB' }
         }
+      },
+      strokeDasharray: {
+        1: 1
+      },
+      strokeDashoffset: {
+        1: 1
       }
     }
   },
