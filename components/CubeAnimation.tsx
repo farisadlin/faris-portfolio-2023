bbb23a386bd768490b1bfb5ce5f4f4a430a24fe4
Faris Nur Adlin <farisadlin@Fariss-MacBook-Pro.local>
2023-05-09 22:21:11 +0700

    feat: added 3D animation

---
+++ components/CubeAnimation.tsx
@@ -0,0 +1,67 @@
+import React from 'react'
+
+import { useRef, useState } from 'react'
+import { Canvas, useFrame, useThree } from '@react-three/fiber'
+import { RenderTexture, OrbitControls, PerspectiveCamera, Text, ContactShadows } from '@react-three/drei'
+
+function Cube() {
+  const textRef = useRef();
+  const meshRef = useRef();
+  useFrame((state) => {
+    textRef.current.position.x = Math.sin(state.clock.elapsedTime) * 2;
+    meshRef.current.rotation.y += 0.01;
+  });
+  return (
+    <mesh ref={meshRef}>
+      <boxGeometry />
+      <meshStandardMaterial>
+        <RenderTexture attach="map" anisotropy={16}>
+          <PerspectiveCamera makeDefault manual aspect={1 / 1} position={[0, 0.5, 5]} />
+          <color attach="background" args={['#D926AA']} />
+          <ambientLight intensity={0.5} />
+          <directionalLight position={[10, 10, 5]} />
+          <Text ref={textRef} fontSize={4} color="white">
+            FA
+          </Text>
+        </RenderTexture>
+      </meshStandardMaterial>
+    </mesh>
+  )
+}
+
+function Dodecahedron(props) {
+  const meshRef = useRef();
+  const [hovered, hover] = useState(false);
+  const [clicked, click] = useState(false);
+  useFrame(() => (meshRef.current.rotation.x += 0.01));
+  return (
+    <group {...props}>
+      <mesh
+        ref={meshRef}
+        scale={clicked ? 1.5 : 1}
+        onClick={() => click(!clicked)}
+        onPointerOver={() => hover(true)}
+        onPointerOut={() => hover(false)}>
+        <dodecahedronGeometry args={[0.75]} />
+        <meshStandardMaterial color={hovered ? 'hotpink' : '#D926AA'} />
+      </mesh>
+    </group>
+  )
+}
+
+
+const CubeAnimation = () => {
+  return (
+    <Canvas camera={{ position: [1, 1, 1], fov: 100 }}>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[10, 10, 5]} />
+      <Cube />
+      <Dodecahedron position={[0, 1, 0]} scale={0.2} />
+      <ContactShadows frames={1} position={[0, -0.5, 0]} blur={1} opacity={0.75} />
+      <ContactShadows frames={1} position={[0, -0.5, 0]} blur={3} color="#D926AA" />
+      <OrbitControls enableZoom={false} enablePan={false} minPolarAngle={0} maxPolarAngle={Math.PI / 2.1} />
+    </Canvas>
+  )
+}
+
+export default CubeAnimation
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,19 +1,14 @@
 import React from 'react'
 import ResumeBtn from './ResumeBtn'
-// import { Canvas } from "@react-three/fiber";
-// import Hexagon from './Hexagon';
+import CubeAnimation from './CubeAnimation'
 
 const Hero = () => {
   return (
     <section className='h-screen flex flex-col justify-center pt-20 lg:mx-20 xl:mx-auto py-28 max-w-7xl xl:max-w-7xl'>
-      {/* Enable this later */}
-      {/* <Canvas>
-        <ambientLight intensity={0.9} />
-        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-        <pointLight position={[-10, -10, -10]} />
-        <Hexagon position={[0, 0, 0]} />
-      </Canvas> */}
-      <div className=''>
+      <div className='z-10 relative'>
+        <div className='absolute left-0 sm:left-1/2 lg:left-3/4 h-full -z-10'>
+          <CubeAnimation />
+        </div>
         <p className='font-fira-code text-secondary mb-5'>Hi, my name is</p>
         <h2 className='text-2xl sm:text-4xl md:text-6xl lg:text-8xl text-zinc-300'>Faris Adlin.</h2>
         <h2 className='text-2xl sm:text-4xl md:text-6xl lg:text-8xl text-zinc-500'>I specialize in web development.</h2>
