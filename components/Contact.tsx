b4be602196e9dba7575c7e13f891e641db4a8fab
farisadlin <farisnura@gmail.com>
2024-08-06 01:20:06 +0700

    fix: fixed button on contact section

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,22 +1,31 @@
-import React from 'react'
-import HeadingFormat from './HeadingFormat'
-import ResumeBtn from './ResumeBtn'
+import React from "react";
+import HeadingFormat from "./HeadingFormat";
+import ResumeBtn from "./ResumeBtn";
 
 const Contact = () => {
+  const handleClick = () => {
+    window.open("mailto:farisnura@gmail.com", "_blank");
+  };
+
   return (
-    <section className='h-screen md:h-half-screen' id='contact'>
+    <section className="h-screen md:h-half-screen" id="contact">
       <HeadingFormat number={4} title={`What's Next?`} disableLine={true} />
-      <p className='text-center text-6xl my-2 text-zinc-200'>Get In Touch</p>
-      <p className='text-center md:w-2/3 lg:w-1/2 mx-auto'>
-      I’m currently looking for any new opportunities and my inbox is always open. Whether you have a question or just want to say hi, I’ll try my best to get back to you!
+      <p className="text-center text-6xl my-2 text-zinc-200">Get In Touch</p>
+      <p className="text-center md:w-2/3 lg:w-1/2 mx-auto">
+        I’m currently looking for any new opportunities and my inbox is always
+        open. Whether you have a question or just want to say hi, I’ll try my
+        best to get back to you!
       </p>
-      <div className='flex justify-center'>
-        <a href='mailto:farisnura@gmail.com' target='_blank' rel='noopener noreferrer'>
-          <ResumeBtn text='Say Hi!' urlDownload='mailto:farisnura@gmail.com' hasResume={false} />
-        </a>
+      <div className="flex justify-center items-center mt-8">
+        <ResumeBtn
+          text="Say Hi!"
+          hasResume={false}
+          noMargin
+          onClick={handleClick}
+        />
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
